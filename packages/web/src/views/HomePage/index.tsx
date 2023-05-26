caa2ebc15efc29e9ad72d7e360ede6dd62cf98ae
yiwwhl <yiwwhl@qq.com>
2023-05-26 22:14:58 +0800

    feat: try modify file with nvim

--- packages/web/src/views/HomePage/index.tsx
+++ packages/web/src/views/HomePage/index.tsx
@@ -2,13 +2,14 @@ import Image from "@/components/Image";
 import SpaceArround from "@/components/SpaceArround";
 import { getImage } from "@/plugins/ImageAutoLoader/ImageCollector";
 import { defineComponent } from "vue";
+import styles from './index.module.scss'
 
 export default defineComponent({
   setup() {
     return () => {
       return (
         <SpaceArround>
-          <Image src={getImage("pokemon")}></Image>
+          <Image class={styles.avatar} src={getImage("pokemon")}></Image>
         </SpaceArround>
       );
     };
